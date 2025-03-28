2e6eb77cc39116c5e1ccef2d4a969cca907e935a
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-03-28 12:39:02 +0900

    commit

--- NLP/LLM/train.py
+++ NLP/LLM/train.py
@@ -1,6 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 from model import GPTModel, CONTEXT_LENGTH
+from tqdm import tqdm
 
 def train_model(model, dataset, epochs, batch_size, lr, device):
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
@@ -12,7 +13,9 @@ def train_model(model, dataset, epochs, batch_size, lr, device):
     for epoch in range(epochs):
         model.train()
         epoch_loss = 0
-        for input_batch, target_batch in train_loader:
+
+        pbar = tqdm(train_loader, desc=f"Epoch {epoch + 1}/{epochs}")
+        for input_batch, target_batch in pbar:
             optimizer.zero_grad()
             input_batch, target_batch = input_batch.to(device), target_batch.to(device)
             logits = model(input_batch)
@@ -23,9 +26,9 @@ def train_model(model, dataset, epochs, batch_size, lr, device):
             tokens_seen += input_batch.numel()
             global_step += 1
             if global_step % 1000 == 0:
-                print(f"Tokens seen: {tokens_seen}")
+                pbar.set_postfix(tokens_seen=tokens_seen, loss=loss.item())
         avg_loss = epoch_loss / len(train_loader)
         losses.append(avg_loss)
-        print(f"Epoch: {epoch + 1}, Loss: {avg_loss}")
+        print(f"Epoch: {epoch + 1}, Average Loss: {avg_loss}")
         torch.save(model.state_dict(), f"model_{str(epoch + 1).zfill(3)}.pth")
     return losses
