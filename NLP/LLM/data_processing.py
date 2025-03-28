65226f84da11241ad6c6d4c8a7a1627139a0f532
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-03-28 12:35:35 +0900

    commit

--- NLP/LLM/data_processing.py
+++ NLP/LLM/data_processing.py
@@ -1,3 +1,4 @@
+import os
 import re
 
 def clean_text(filename):
@@ -7,9 +8,10 @@ def clean_text(filename):
     cleaned_text = re.sub(r'\n+', ' ', book_text)  # 줄바꿈을 빈칸으로 변경
     cleaned_text = re.sub(r'\s+', ' ', cleaned_text)  # 여러 빈칸을 하나로
 
-    output_filename = "cleaned_" + filename
+    base_name = os.path.basename(filename)
+    output_filename = "cleaned_" + base_name
     print(output_filename, len(cleaned_text), "characters")
-    
+
     with open(output_filename, 'w', encoding='utf-8') as file:
         file.write(cleaned_text)
 
