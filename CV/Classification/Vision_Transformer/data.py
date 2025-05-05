cdd27af55b2e506ad548c1d72875e1885822d3dc
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-05-05 20:43:13 +0900

    commit

--- CV/Classification/Vision_Transformer/data.py
+++ CV/Classification/Vision_Transformer/data.py
@@ -12,7 +12,6 @@ def cifar_10(batch_size=64):
         transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
     ])
 
-
     test_transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
